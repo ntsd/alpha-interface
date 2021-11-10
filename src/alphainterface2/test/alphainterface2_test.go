7aa99daa0dcc86f0942ebe4bfb0cdf5a292c5bab
Jirawat Boonkumnerd <jo06942@gmail.com>
2021-11-10 19:20:35 +0700

    feat: add contract version 2

---
+++ src/alphainterface2/test/alphainterface2_test.go
@@ -0,0 +1,14 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/iotaledger/wasp/alphainterface/src/contract2/go/alphainterface2"
+	"github.com/iotaledger/wasp/packages/vm/wasmsolo"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDeploy(t *testing.T) {
+	ctx := wasmsolo.NewSoloContext(t, alphainterface2.ScName, alphainterface2.OnLoad)
+	require.NoError(t, ctx.ContractExists(alphainterface2.ScName))
+}
